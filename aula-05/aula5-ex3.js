985fba5360e7dfde57dc436a84f214fcc23cb9a4
Gustavo Muraro <gus@gustavomuraro.com>
2021-06-10 17:06:47 -0300

    Exercícios da aula 05

---
+++ aula-05/aula5-ex3.js
@@ -0,0 +1,14 @@
+let frutas=['banana', 'melão'];
+frutas.push('abacaxi', 'cereja');
+console.log(frutas);
+console.log(frutas[2]);
+
+//removendo a cereja
+console.log(frutas.pop());
+console.log(frutas);
+
+let frutaRemovida = frutas.pop();
+console.log(frutas);
+frutas.shift();
+
+console.log(frutas);
