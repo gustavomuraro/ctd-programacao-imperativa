fddac3082aa6fed4a697d0eb9d14f277f56be9ae
Gustavo Muraro <gus@gustavomuraro.com>
2021-06-22 07:45:47 -0300

    Atualização exercício meucardapio

--- aula-08/meucardapio.js
+++ aula-08/meucardapio.js
@@ -1,11 +1,15 @@
 //Crie uma função construtora chamada Restaurante, que receba como parâmetros o nome do restaurante e o cardápio (que será um array de strings).
-// Após isso chame a função entrada() dentro do objeto, que irá trazer uma mensagem de boas-vindas, informando o nome do restaurante, e o cardápio.
+//Após isso chame a função entrada() dentro do objeto, que irá trazer uma mensagem de boas-vindas, informando o nome do restaurante, e o cardápio.
 
 function Restaurante(nomeRestaurante, cardapio) {
   this.nomeRestaurante = nomeRestaurante;
   this.cardapio = cardapio;
 };
 
+//Para criar um objeto com a função construtora acima, deve-se  usar a palavra-chave new e chamar a função passando os parâmetros esperados.
+//let meuRestaurante = new Restaurante('Pizza Planet', [' marguerita', ' portuguesa', ' pepperoni', ' napolitana']);
+
+//Cria um objeto com duas propriedades e um método que retorna uma mensagem de boas-vindas:
 let meuRestaurante = {
   nomeRestaurante: 'Pizza Planet',
   cardapio: [' marguerita', ' portuguesa', ' pepperoni', ' napolitana'],
@@ -14,4 +18,5 @@ let meuRestaurante = {
   }
 }
 
+//Invoca o método do objeto meuRestaurante: 
 console.log(meuRestaurante.boasvindas())
