ff9cd68d2ad139f3f4dface02ebe0b762ed8d405
Gustavo Muraro <gus@gustavomuraro.com>
2021-06-24 13:54:44 -0300

    Exercícios aula 09

---
+++ aula-09/importacoes.js
@@ -0,0 +1,5 @@
+//Crie um módulo que exporte uma função (em um arquivo separado). Após isso, importe o módulo no arquivo principal e execute a função importada.
+
+let minhaHoraData = require('./myfirstmodule');
+
+console.log(minhaHoraData);
---
+++ aula-09/myfirstmodule.js
@@ -0,0 +1,7 @@
+// Cria um módulo que retorna a data e hora atual
+
+let minhaHoraData = function () {
+  return Date();
+}
+
+module.exports = minhaHoraData
