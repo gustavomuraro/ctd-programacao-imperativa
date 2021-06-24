319ab9f3cd34e48f73340b52ddd9fe62079f46ae
Gustavo Muraro <gus@gustavomuraro.com>
2021-06-24 15:49:46 -0300

    Exercícios 5 e 6 aula síncrona

---
+++ aula-09/aula9ex5.js
@@ -0,0 +1,4 @@
+//Exporta vários módulos no mesmo arquivo
+exports.somar = (a,b)=>a+b;
+exports.subtrair = (a,b)=>a-b;
+exports.multiplicar = (a,b)=>a*b;
---
+++ aula-09/aula9ex6.js
@@ -0,0 +1,7 @@
+//Importa o módulo do arquivo aula9ex5.js
+const calculo=require('./aula9ex5');
+
+//Testes
+console.log(calculo.somar(1,2));
+console.log(calculo.subtrair(10,2));
+console.log(calculo.multiplicar(10,2));
